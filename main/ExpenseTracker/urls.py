84e52c7c9f8cde4c26676358c093ec801868aefe
github-ashwin <ashi.github@gmail.com>
2025-07-10 23:26:04 +0530

    Added get api

--- main/ExpenseTracker/urls.py
+++ main/ExpenseTracker/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from api import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('expenses/',views.ExpenseView.as_view())
 ]
--- main/api/views.py
+++ main/api/views.py
@@ -4,4 +4,17 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import *
 from .serializers import *
+from rest_framework import status
 # Create your views here.
+
+class ExpenseView(APIView):
+
+    serializer_class = ExpenseSerializer
+
+    def get(self,request,*args, **kwargs):
+
+        query_set = Expense.objects.all()
+
+        serializer = self.serializer_class(query_set,many=True)
+
+        return Response(data=serializer.data)
